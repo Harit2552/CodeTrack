87724ad6383e204d50b40c94c258be021180f2cb
Amanparashar-09 <amanparashar0911@gmail.com>
2026-03-26 19:32:59 +0530

    feat: add auth middleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id).select("-password");
+    if (!user) {
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { protect };
